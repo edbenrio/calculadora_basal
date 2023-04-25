0f830c81f886cbb5234877c1464a7f2b2f3d7dc6
edbenrio <edgarrubendario@hotmail.com>
2023-04-24 20:05:15 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -1,44 +1,67 @@
+const INPUT = document.getElementById("peso");
+INPUT.focus();
+const ERROR = document.getElementById("error");
 const CALCULAR = document.getElementById("calcular");
+
 CALCULAR.addEventListener("click", ()=>{
-    const ERROR = document.getElementById("error");
-    
-    const INPUT = document.getElementById("peso");
     const PESO = INPUT.value;
     if(PESO > 0){
         ERROR.style.display = 'none';
-        const restultado = elegirMetodo(PESO);
-        mostrarResultado(restultado);
+        elegirMetodo(PESO);
     }else{
         ERROR.style.display = 'block';
     }
+});
 
-    function elegirMetodo(peso){
-        if(peso <= 30){
-            return calculoHS(peso);
+INPUT.addEventListener("keypress", function(event) {
+    if (event.key === "Enter") {
+        const PESO = INPUT.value;
+        if(PESO > 0){
+            ERROR.style.display = 'none';
+            elegirMetodo(PESO);
         }else{
-            return calculoSC(peso);
+            ERROR.style.display = 'block';
         }
-    }
-
-    function calculoHS(peso){
-        if(peso <= 10){
-            return 100 * peso;
-        }
-        if(peso > 10 && peso < 20){
-            return ((peso - 10) * 50) + 1000;
-        }
-        if(peso > 20){
-            return ((peso - 20) * 20) + 1500;
-        }
-    }
-
-    function calculoSC(peso){
-        return ( (peso * 4) + 7) / (peso + 90)
-    }
-
-    function mostrarResultado(restultado){
-        const h3 = document.getElementById("resultado");
-        h3.innerText = restultado + 'cc / día';
-        console.log(restultado);
     }
 });
+
+function elegirMetodo(peso){
+    if(peso <= 30){
+        return calculoHS(peso);
+    }else{
+        return calculoSC(peso);
+    }
+}
+
+function calculoHS(peso){
+    let flujo = 0;
+    if(peso <= 10){
+        flujo = 100 * peso;
+    }
+    if(peso > 10 && peso < 20){
+        flujo = ((peso - 10) * 50) + 1000;
+    }
+    if(peso > 20){
+        flujo = ((peso - 20) * 20) + 1500;
+    }
+    const FLUJO_HORA = flujo / 24;
+    const MANTENIMIENTO_MAS_MEDIO = FLUJO_HORA + (FLUJO_HORA / 2);
+    const TABLA = '<h4>Holliday-Segar</h4><table> <thead> <tr> <th>Peso</th> <th>Volumen diario</th> <th>Mantenimiento</th> <th>m+m/2</th> </tr> </thead> <tbody> ' + 
+        '<tr> <td>' + peso + ' Kg</td> <td>' + flujo.toFixed(2) + ' cc</td> <td>' + FLUJO_HORA.toFixed(2) + ' cc/hr</td> <td>' + MANTENIMIENTO_MAS_MEDIO.toFixed(2) + ' cc/hr</td>  </tbody> </table>';
+    mostrarResultado(TABLA);
+}
+
+function calculoSC(peso){
+    const PESO = parseFloat(peso);
+    const FLUJO = (((PESO * 4) + 7) / (PESO + 90));
+    const FLUJO_1500 = (FLUJO * 1500) / 24;
+    const FLUJO_2000 = (FLUJO * 2000) / 24;
+    const TABLA = '<h4>Superficie Corporal</h4><table> <thead> <tr> <th>Peso</th> <th>Mantenimiento * 1500</th> <th>Mantenimiento * 2000</th> </tr> </thead> <tbody> ' + 
+        '<tr> <td>' + peso + ' Kg</td> <td>' + FLUJO_1500.toFixed(2) + ' cc/hr</td> <td>' + FLUJO_2000.toFixed(2) + ' cc/hr</td>  </tbody> </table>';
+    mostrarResultado(TABLA);
+}
+
+function mostrarResultado(restultado){
+    const divResultado = document.getElementById("resultado");
+    divResultado.innerHTML = restultado;
+}
